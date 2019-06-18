ee27cdf18f8fbc68e2957bb2604be1b88ea8070c
ansumana80 <ansurogersbusiness@gmail.com>
2019-06-18 09:54:59 -0400

    first commit

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -1,6 +1,10 @@
-public class HelloWorldMain {
-public static void main(String []args) {
-    System.out.println("hello world");
+public class HelloWorldMain
+{
+public static void main(String []args)
+{
+    System.out.println("Hello, World!");
+    System.out.println("My name is Ansu Rogers!");
+    System.out.println("Today is July 19, 2015. I am alive!");
 
 }
 }
